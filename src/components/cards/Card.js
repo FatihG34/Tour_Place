f2e2f975e9dec5a7b6460136369443207971720d
FatihG34 <fthgnydn34@gmail.com>
2022-06-25 07:28:35 +0300

    navbar and header added

---
+++ src/components/cards/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='gird_container'>
+            <div className='cards'>
+                <h2>gh</h2>
+                <img src="" alt="" />
+                <p>fgh</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className='nav_container'>
+
+            <div><a href="first_react_project\public\index.html">ABOUT US</a></div>
+            <div><a href="#">FOR YOU</a></div>
+            <div><a href="#">SERVICES</a></div>
+            <div><a href="#">BLOG</a></div>
+            <div><a href="#">VLOG</a></div>
+            <div><a href="#">CONTACT</a></div>
+
+        </nav>
+    )
+}
+
+export default Navbar
